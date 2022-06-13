1bb2f4ecabd7499aee6c70e1341a64f1cf8e5672
percyscl <48094439+percyscl@users.noreply.github.com>
2022-06-12 20:17:50 -0500

    commit fin

--- src/main/java/com/condori/vista/FrmCrudLibros.java
+++ src/main/java/com/condori/vista/FrmCrudLibros.java
@@ -285,4 +285,28 @@ public class FrmCrudLibros extends JFrame implements ActionListener, MouseListen
 		}
 		
 	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
